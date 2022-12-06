e22c7fdbea516d8d84fd977205b33fd0497bcd36
Cristian <ctomatfl@gmail.com>
2022-12-06 12:26:01 -0400

    Enable function buttons on the control panel

--- script.js
+++ script.js
@@ -5,6 +5,52 @@ gridContainer.style.gridTemplateRows = "repeat(16, 1fr)";
 
 const slider = document.querySelector(".slider");
 const slideCounter = document.querySelector(".slide-counter");
+const colorPicker = document.querySelector("#color-picker");
+
+let gridDimension = 16;
+let brushColor = "#DBA39A";
+let colorModeEnable = true;
+let rainbowModeEnable = false;
+let eraserEnable = false;
+
+const buttons = {
+  colorMode: document.querySelector("#color-mode"),
+  rainbowMode: document.querySelector("#rainbow-mode"),
+  eraser: document.querySelector("#eraser"),
+  clear: document.querySelector("#clear"),
+};
+
+colorPicker.addEventListener(
+  "input",
+  (event) => (brushColor = event.target.value)
+);
+
+buttons.colorMode.addEventListener("click", () => {
+  buttons.rainbowMode.classList.remove("button-press");
+  buttons.colorMode.classList.add("button-press");
+  buttons.eraser.classList.remove("button-press");
+  colorModeEnable = true;
+  rainbowModeEnable = false;
+  eraserEnable = false;
+});
+
+buttons.rainbowMode.addEventListener("click", () => {
+  buttons.rainbowMode.classList.add("button-press");
+  buttons.colorMode.classList.remove("button-press");
+  buttons.eraser.classList.remove("button-press");
+  colorModeEnable = false;
+  rainbowModeEnable = true;
+  eraserEnable = false;
+});
+
+buttons.eraser.addEventListener("click", () => {
+  buttons.rainbowMode.classList.remove("button-press");
+  buttons.colorMode.classList.remove("button-press");
+  buttons.eraser.classList.add("button-press");
+  colorModeEnable = false;
+  rainbowModeEnable = false;
+  eraserEnable = true;
+});
 
 function createGrid(gridDimension = 16) {
   for (let index = 0; index < gridDimension ** 2; index++) {
@@ -25,17 +71,52 @@ function createGrid(gridDimension = 16) {
     });
 
     grid.addEventListener("mousedown", () => {
-      grid.style.backgroundColor = "red";
+      if (colorModeEnable) {
+        grid.style.backgroundColor = brushColor;
+      }
+
+      if (rainbowModeEnable) {
+        grid.style.backgroundColor =
+          "#" + Math.floor(Math.random() * 16777215).toString(16);
+      }
+
+      if (eraserEnable) {
+        grid.style.backgroundColor = "white";
+      }
     });
 
     grid.addEventListener("mousemove", () => {
-      if (hold === true) {
-        grid.style.backgroundColor = "red";
+      if (!hold) {
+        return;
+      }
+
+      if (colorModeEnable) {
+        grid.style.backgroundColor = brushColor;
+        return;
+      }
+
+      if (rainbowModeEnable) {
+        grid.style.backgroundColor =
+          "#" + Math.floor(Math.random() * 16777215).toString(16);
+      }
+
+      if (eraserEnable) {
+        grid.style.backgroundColor = "white";
       }
     });
 
     gridContainer.appendChild(grid);
   }
+}
+
+function clearGrid() {
+  const grid = document.querySelectorAll(".grid");
+  grid.forEach((node) => {
+    node.parentNode.removeChild(node);
+  });
+
+  gridContainer.style.gridTemplateColumns = `repeat(${gridDimension}, 1fr)`;
+  gridContainer.style.gridTemplateRows = `repeat(${gridDimension}, 1fr)`;
 }
 
 createGrid();
@@ -45,15 +126,13 @@ slider.addEventListener("input", (event) => {
 });
 
 slider.addEventListener("change", (event) => {
-  const gridDimension = event.target.value;
+  gridDimension = event.target.value;
 
-  const grid = document.querySelectorAll(".grid");
-  grid.forEach((node) => {
-    node.parentNode.removeChild(node);
-  });
+  clearGrid(gridDimension);
+  createGrid(gridDimension);
+});
 
-  gridContainer.style.gridTemplateColumns = `repeat(${event.target.value}, 1fr)`;
-  gridContainer.style.gridTemplateRows = `repeat(${event.target.value}, 1fr)`;
-
+buttons.clear.addEventListener("click", () => {
+  clearGrid(gridDimension);
   createGrid(gridDimension);
 });
